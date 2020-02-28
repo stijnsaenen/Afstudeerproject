e07286efa7ae68b074470b92cc575c00fb85af8d
Pieter-JanVanDerSchueren-r0465971 <32223183+Pieter-JanVanDerSchueren-r0465971@users.noreply.github.com>
2020-02-28 14:23:08 +0100

    changes

--- Flask/static/js/network.js
+++ Flask/static/js/network.js
@@ -1,23 +1,20 @@
-
-
-
 function getJson() {
-            var res = function () {
-                var result = null;
-                $.ajax({
-                    type: "GET",
-                    async: false,
-                    global: false,
-                    url: 'http://localhost:5000/all',
-                    success: function (data) {
-                        result = data;
-                    }
-                });
-                return result
+    var res = function () {
+        var result = null;
+        $.ajax({
+            type: "GET",
+            async: false,
+            global: false,
+            url: 'http://localhost:5000/all',
+            success: function (data) {
+                result = data;
+            }
+        });
+        return result
 
-            }();
-            return res
-        }
+    }();
+    return res
+}
 
 var width = 1165,
     height = 600
@@ -47,81 +44,90 @@ var force = d3.layout.force()
     .size([width, height]);
 
 var json = getJson();
-        console.log(json);
+console.log(json);
 
- var nodeById = d3.map();
+var nodeById = d3.map();
 
-            json.nodes.forEach(function(node) {
-                nodeById.set(node.ContactId, node);
-            });
+json.nodes.forEach(function (node) {
+    nodeById.set(node.ContactId, node);
+});
 
-            json.links.forEach(function(link) {
-                link.source = nodeById.get(link.source);
-                link.target = nodeById.get(link.target);
-            });
-    force
-        .nodes(json.nodes)
-        .links(json.links)
-        .start();
+json.links.forEach(function (link) {
+    link.source = nodeById.get(link.source);
+    link.target = nodeById.get(link.target);
+});
+force
+    .nodes(json.nodes)
+    .links(json.links)
+    .start();
 
-    var link = svg.selectAll(".link")
-        .data(json.links)
-        .enter().append("line")
-        .attr("class", "link")
-        .style("stroke-width", function (d) {
-            return Math.sqrt(d.weight);
-        });
-
-    var node = svg.selectAll(".node")
-        .data(json.nodes)
-        .enter().append("g")
-        .attr("class", "node")
-        .on("click", smaller)
-        .call(force.drag);
-
-    node.append("circle")
-        .attr("r", "17");
-
-    node.append("text")
-        .attr("dx", 0)
-        .attr("dy", ".35em")
-        .attr("y", -25)
-        .style("text-anchor", "middle")
-        .text(function (d) {
-            return d.ContactName
-        });
-
-    var tip;
-    var xSpacing = 25;
-    var ySpacing = 40;
-
-    function sendInfo(d) {
-        showInfo(d);
-    }
-
-    node.on("mouseover", function (d) {
-        sendInfo(d);
+var link = svg.selectAll(".link")
+    .data(json.links)
+    .enter().append("line")
+    .attr("class", "link")
+    .style("stroke-width", function (d) {
+        return Math.sqrt(d.weight);
     });
 
+var node = svg.selectAll(".node")
+    .data(json.nodes)
+    .enter().append("g")
+    .attr("class", "node")
+    .on("click", smaller)
+    .call(force.drag);
 
-    force.on("tick", function () {
-        link.attr("x1", function (d) {
+node.append("circle")
+    .attr("r", "17");
+/*
+link.append("text")
+    .attr("dx", 0)
+    .attr("dy", ".35em")
+    .attr("y", -25)
+    .style("text-anchor", "middle")
+    .text(function (l) {
+        return l.LeftcontactTitle
+    });*/
+
+node.append("text")
+    .attr("dx", 0)
+    .attr("dy", ".35em")
+    .attr("y", -25)
+    .style("text-anchor", "middle")
+    .text(function (d) {
+        return d.ContactName
+    });
+
+var tip;
+var xSpacing = 25;
+var ySpacing = 40;
+
+function sendInfo(d) {
+    showInfo(d);
+}
+
+node.on("mouseover", function (d) {
+    sendInfo(d);
+});
+
+
+force.on("tick", function () {
+    link.attr("x1", function (d) {
             return d.source.x;
         })
-            .attr("y1", function (d) {
-                return d.source.y;
-            })
-            .attr("x2", function (d) {
-                return d.target.x;
-            })
-            .attr("y2", function (d) {
-                return d.target.y;
-            });
-
-        node.attr("transform", function (d) {
-            return "translate(" + d.x + "," + d.y + ")";
+        .attr("y1", function (d) {
+            return d.source.y;
+        })
+        .attr("x2", function (d) {
+            return d.target.x;
+        })
+        .attr("y2", function (d) {
+            return d.target.y;
         });
+
+    node.attr("transform", function (d) {
+        return "translate(" + d.x + "," + d.y + ")";
     });
+});
 
 
 
@@ -143,40 +149,16 @@ function showInfo(d) {
 
     var i = 1;
     d3.keys(d).forEach(printText)
-    function printText(key) {
-        //if (key != ("index") && key !=("weight") && key != ("x") && key != ("y") && key != ("px") && key != ("py") && key != ("fixed")) {
-        d3.select('.info').append("text")
-            .attr("dy", i + "em")
-            .attr("dx", 5)
-            .attr("text-anchor", "start")
-            .text(key + ": " + d[key])
-        i++;
-        //}
 
+    function printText(key) {
+        if (key != ("index") && key != ("weight") && key != ("x") && key != ("y") && key != ("px") && key != ("py") && key != ("fixed") && d[key] != null && d[key] != '') {
+            d3.select('.info').append("text")
+                .attr("dy", i + "em")
+                .attr("dx", 5)
+                .attr("text-anchor", "start")
+                .text(key + ": " + d[key])
+            i++;
+        }
     }
     var infoBbox = d3.select('.info').node().getBBox()
 };
-
-
-var legendInfo = d3.select('#area3').append("svg")
-    .attr("width", (4 / 7) * width * 0.8)
-    .attr("height", height * 0.3)
-
-var legendArea = legendInfo.append("g")
-    .attr('class', 'legend')
-
-var rect = d3.select('.legend').append("rect")
-    .style("fill", "white")
-    .style("stroke", "black")
-
-var text1 = d3.select('.legend').append("text")
-    .attr("dy", "1em")
-    .attr("dx", 5)
-    .attr("text-anchor", "start")
-    .text("Placeholder legende")
-
-var legendBox = d3.select('.legend').node().getBBox()
-rect.attr("x", legendBox.x - 5)
-    .attr("y", legendBox.y)
-    .attr("width", (4 / 7) * width * 0.65)
-    .attr("height", height * 0.24)
